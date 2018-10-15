732a53eb9744fb65b8cacd830b5049229f40448e
Nathan Prigent <prigent.nathan@gmail.com>
2018-10-15 15:43:47 +0200

    OK

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,26 @@
 // on écoute le clik sur les elements qui ont la classe nav-items
-$('.nav-item').click(function() { 
+$('.nav-item').click(function(event) { 
     $('.nav-item').removeClass('active');
     $(this).addClass('active');  
 });
-       
+
+
+var MyTexteSecret = " Devenir Web developpeur et Expert Wordpress";
+
+
+
+var myArray = MyTexteSecret.split("");
+
+var timeloop;
+
+function loop(){
+    if(myArray.length > 0){
+        document.getElementById("secretary_test").innerHTML += myArray.shift();
+    }else{
+        clearTimeout(timeloop);
+    }
+
+    timeloop = set setTimeout('loop()', 70);
+
+}
+loop();
